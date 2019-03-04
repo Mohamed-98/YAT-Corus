0c03fc1c06646a3d6a904685ef8e05e3ce433ced
Mohamed-98 <m.gado86@gmail.com>
2019-03-04 15:03:56 +0200

    day 10

--- front-end/javascript/js/function.js
+++ front-end/javascript/js/function.js
@@ -1,6 +1,9 @@
 // Define (declaration)
 	  
-function addTwoNumbers() // camel case 
+function addTwoNumbers(firstNumber,secondNumber) // camel case 
 {
-	document.write(5+7);
+	//document.write(5+7);
+	//document.write(firstNumber+secondNumber);
+	/var result = firstNumber + secondNumber ;
+	return result;
 }
---
+++ front-end/jquery/js/script.js
@@ -0,0 +1,11 @@
+// Syntax 
+// $(selector).action(); 
+$(document).ready(function (){
+	//$('p').hide(3000);
+	//$('#welcome').hide(3000);
+	// chaning 
+	for(var l=1;l<=3;l++){
+		$('.jq').hide(3000).delay(2000).show(3000);
+	}
+});
+	
