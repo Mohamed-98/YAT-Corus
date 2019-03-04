d7c40e3742d02876ce5df577f34c752380dd69e4
Mohamed-98 <m.gado86@gmail.com>
2019-03-04 14:59:50 +0200

    day 9

--- front-end/javascript/js/login.js
+++ front-end/javascript/js/login.js
@@ -4,12 +4,12 @@ function ckeckLogin()
    var us_name = document.getElementById("username").value,
 	us_pass = document.getElementById("password").value ;
    if( us_name =='admin' && us_pass == '123' )
-  {
-	  // user redirect 
-	  window.location = 'https://www.google.com';
-  }
-  else
-  {
-	  document.getElementById("errMsg").innerHTML = "Invalid username or password";
-  }
+	  {
+		  // user redirect 
+		  window.location = 'https://www.google.com';
+	  }
+	  else
+	  {
+		  document.getElementById("errMsg").innerHTML = "Invalid username or password";
+	  }
  }
