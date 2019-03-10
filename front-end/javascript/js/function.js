480317a00834a719d660209eef6678f3ee8f513e
Mohamed-98 <m.gado86@gmail.com>
2019-03-10 04:16:49 +0200

    Redone

--- front-end/javascript/js/function.js
+++ front-end/javascript/js/function.js
@@ -4,6 +4,6 @@ function addTwoNumbers(firstNumber,secondNumber) // camel case
 {
 	//document.write(5+7);
 	//document.write(firstNumber+secondNumber);
-	/var result = firstNumber + secondNumber ;
+	var result = firstNumber + secondNumber ;
 	return result;
 }
